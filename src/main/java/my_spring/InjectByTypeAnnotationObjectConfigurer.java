d908b38e8b7c4be9a7e7e620e590e888aa016324
NikitinaOlga <https://github.com/Stulnikitina/pracprac>
2020-09-08 11:58:48 +0300

    7 day

---
+++ src/main/java/my_spring/InjectByTypeAnnotationObjectConfigurer.java
@@ -0,0 +1,21 @@
+package my_spring;
+
+import lombok.SneakyThrows;
+
+import java.lang.reflect.Field;
+
+public class InjectByTypeAnnotationObjectConfigurer implements ObjectConfigurer {
+    @SneakyThrows
+    @Override
+    public void configure(Object t, ApplicationContext context) {
+        Field[] fields = t.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            if (field.isAnnotationPresent(InjectByType.class)) {
+                Object value = context.getBean(field.getType());
+                field.setAccessible(true);
+                field.set(t,value);
+            }
+        }
+    }
+
+}
